498a6b548261eb39d8c6ea3cdb2d0bfb9c197028
FVComynoi <felipecomynoi@gmail.com>
2026-05-19 15:26:45 -0300

    Implemented run and attack anim
    -Added run animation
    -Added Attack animation
    -Sliced Sprite Sheet
    -Created the player behaviour script

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    
+    private InputAction move, attack;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        //move =
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
