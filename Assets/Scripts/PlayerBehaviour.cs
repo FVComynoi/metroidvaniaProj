2fa1331f2e1c3e6bd173b6d943190a11e2726266
FVComynoi <felipecomynoi@gmail.com>
2026-05-25 21:15:01 -0300

    Implemented something?

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,23 +4,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    
+    InputSystem_Actions inputSystemActions;
     private InputAction move, attack;
+    Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        //move =
+        rb = GetComponent<Rigidbody>();
+        move = inputSystemActions.Player.Move;
     }
-
+    private void OnEnable()
+    {
+        move.Enable();
+    }
+    private void OnDisable()
+    {
+        move.Disable();
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
